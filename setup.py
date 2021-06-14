ea2350acc4a43351225c2930adff81eda6b406f3
SwissCorePy <51398037+SwissCorePy@users.noreply.github.com>
2021-06-14 23:16:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='pyTelegramBotCAPTCHA',
     packages=['pyTelegramBotCAPTCHA'],
-    version='0.0.3',
+    version='0.0.4',
     license='gpl-3.0',
     description='An easy to use an (hopefully useful) captcha solution for pyTelegramBotAPI.',
     author='SwissCorePy',
