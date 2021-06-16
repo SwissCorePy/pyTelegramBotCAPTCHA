7e0d2a744852b377d05805557649be73776244bd
SwissCorePy <51398037+SwissCorePy@users.noreply.github.com>
2021-06-16 16:38:39 +0200

    Create captcha_test_bot.py
    A working template bot to test around with CAPTCHAs using /test command

---
+++ examples/captcha_test_bot.py
@@ -0,0 +1,38 @@
+from telebot import TeleBot
+from pyTelegramBotCAPTCHA import CaptchaManager
+                                                                    
+bot = TeleBot("TOKEN")
+captcha_manager = CaptchaManager(bot.get_me().id)
+
+# Test command handler
+@bot.message_handler(commands=["test"])
+def test_captcha(message):
+    captcha_manager.send_random_captcha(bot, message.chat, message.from_user)
+                                                                    
+# Callback query handler
+@bot.callback_query_handler(func=lambda callback:True)
+def on_callback(callback):
+  captcha_manager.update_captcha(bot, callback)
+                                                                    
+# Handler for correct solved CAPTCHAs
+@captcha_manager.on_captcha_correct
+def on_correct(captcha):
+  bot.send_message(captcha.chat.id, "Congrats! You solved the CAPTCHA!")
+  captcha_manager.delete_captcha(bot, captcha)
+
+# Handler for wrong solved CAPTCHAs
+@captcha_manager.on_captcha_not_correct
+def on_not_correct(captcha):
+  if (captcha.incorrect_digits == 1 and captcha.previous_tries < 2):
+    captcha_manager.refresh_captcha(bot, captcha)
+  else:
+    bot.send_message(captcha.chat.id, f"You failed solving the CAPTCHA!")
+    captcha_manager.delete_captcha(bot, captcha)
+  
+# Handler for timed out CAPTCHAS
+@captcha_manager.on_captcha_timeout
+def on_timeout(captcha):
+  bot.send_message(captcha.chat.id, f"You did not solve the CAPTCHA!")
+  captcha_manager.delete_captcha(bot, captcha)
+  
+bot.polling()
