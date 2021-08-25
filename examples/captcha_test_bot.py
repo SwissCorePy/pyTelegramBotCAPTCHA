f9e412ad165467595c753b0a2553df0e9ca99854
SwissCorePy <51398037+SwissCorePy@users.noreply.github.com>
2021-08-25 17:05:01 +0200

    Added new generator and better options

--- examples/captcha_test_bot.py
+++ examples/captcha_test_bot.py
@@ -1,13 +1,21 @@
 from telebot import TeleBot
-from pyTelegramBotCAPTCHA import CaptchaManager
-                                                                    
-bot = TeleBot("TOKEN")
-captcha_manager = CaptchaManager(bot.get_me().id)
+from pyTelegramBotCAPTCHA import CaptchaManager, CaptchaOptions, CustomLanguage
+
+token = open("/Volumes/Macintosh SSD/Users/fredi/Desktop/test_token.txt", "r").read().strip()
+bot = TeleBot(token)
+
+options = CaptchaOptions()
+options.generator = "keyZenD"
+options.max_user_reloads = 2
+options.timeout = 120
+options.language = "uz"
+
+captcha_manager = CaptchaManager(bot.get_me().id, default_options=options)
 
 # Test command handler
 @bot.message_handler(commands=["test"])
 def test_captcha(message):
-    captcha_manager.send_random_captcha(bot, message.chat, message.from_user)
+    captcha_manager.send_new_captcha(bot, message.chat, message.from_user)
                                                                     
 # Callback query handler
 @bot.callback_query_handler(func=lambda callback:True)
@@ -17,22 +25,19 @@ def on_callback(callback):
 # Handler for correct solved CAPTCHAs
 @captcha_manager.on_captcha_correct
 def on_correct(captcha):
-  bot.send_message(captcha.chat.id, "Congrats! You solved the CAPTCHA!")
+  bot.send_message(captcha.chat.id, "✅ : Congrats! You solved the CAPTCHA!")
   captcha_manager.delete_captcha(bot, captcha)
 
 # Handler for wrong solved CAPTCHAs
 @captcha_manager.on_captcha_not_correct
-def on_not_correct(captcha):
-  if (captcha.incorrect_digits == 1 and captcha.previous_tries < 2):
-    captcha_manager.refresh_captcha(bot, captcha)
-  else:
-    bot.send_message(captcha.chat.id, f"You failed solving the CAPTCHA!")
-    captcha_manager.delete_captcha(bot, captcha)
+def on_not_correct(captcha):  
+  bot.send_message(captcha.chat.id, f"❌ : You failed solving the CAPTCHA!")
+  captcha_manager.delete_captcha(bot, captcha)
   
 # Handler for timed out CAPTCHAS
 @captcha_manager.on_captcha_timeout
 def on_timeout(captcha):
-  bot.send_message(captcha.chat.id, f"You did not solve the CAPTCHA!")
+  bot.send_message(captcha.chat.id, f"❌ : You did not solve the CAPTCHA!")
   captcha_manager.delete_captcha(bot, captcha)
   
 bot.polling()
--- pyTelegramBotCaptcha/__init__.py
+++ pyTelegramBotCaptcha/__init__.py
@@ -1,4 +1,4 @@
-from .telebot_captcha import CaptchaManager, Captcha
+from .telebot_captcha import CaptchaManager, Captcha, CaptchaOptions, CustomLanguage
 
 
 __author__ = "SwissCorePy"
