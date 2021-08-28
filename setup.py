bb81c887e6f3d5ff5aa9003ad2f4908513dfbfff
SwissCorePy <51398037+SwissCorePy@users.noreply.github.com>
2021-08-28 19:48:03 +0200

    Update to 1.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name='pyTelegramBotCAPTCHA',
     packages=['pyTelegramBotCAPTCHA'],
-    version='1.1.0',
+    version='1.1.1',
     author='SwissCorePy',
     author_email='swisscore.py@gmail.com',
 
