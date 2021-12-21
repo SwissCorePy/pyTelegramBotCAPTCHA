827f4d7dd20cea08b6fb279802fdf36ca7a35e6d
_run <khumogo1@gmail.com>
2021-12-21 21:43:07 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name='pyTelegramBotCAPTCHA',
     packages=['pyTelegramBotCAPTCHA'],
-    version='1.1.2',
+    version='1.1.3',
     author='SwissCorePy',
     author_email='swisscore.py@gmail.com',
 
